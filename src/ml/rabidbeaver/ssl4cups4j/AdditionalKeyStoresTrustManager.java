8b5809d3a7277b4a1e4a26c70e99c65637d758ce
lbdroid <definesinsanity@gmail.com>
2015-04-24 09:13:07 -0400

    Cleanup

--- src/ml/rabidbeaver/ssl4cups4j/AdditionalKeyStoresTrustManager.java
+++ src/ml/rabidbeaver/ssl4cups4j/AdditionalKeyStoresTrustManager.java
@@ -1,23 +1,5 @@
 package ml.rabidbeaver.ssl4cups4j;
 
-/*
-JfCupsPrintService
-Copyright (C) 2014 Jon Freeman
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 //This class based on http://stackoverflow.com/questions/2642777/trusting-all-certificates-using-httpclient-over-https
 
 import java.security.KeyStore;
